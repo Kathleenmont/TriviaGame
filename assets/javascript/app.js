d8974c10bb11a4ee99d4dcd90a31c62527bd71fd
Kathleen Montesano <kathleen.m.montesano@gmail.com>
2019-03-08 12:08:22 -0500

    timer fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 // name some global variables 
 var images = ["assets/images/peppermint.gif", "assets/images/alyssaReaction.gif", "assets/images/needles.gif", "assets/images/latrice.gif", "assets/images/vanjie.gif", "assets/images/bobz.gif", "assets/images/shea.gif"];
 var questions = ["Which queen was the season 9 runner up?", "Which queen went as Joan Crawford in The Snatch Game?", "In the episode 'RuPocalypse Now!' who was the winner of the post-apocalyptic runway challange?", 'Which Queen can we attribut the catchphrase "The shade of it all" to?', 'Which queen won the lip sync that sent home Vanessa Vanjie Mateo but gave the world the famous "Miss Vanjie" exit?', "Which queen is Mis Cracker's Drag Mother? ", "Which queen won more challenges then her seasons winner did?"];
-var possibleAnswers = [["Trinity 'The Tuck' Taylor", "Shea Couleé", "Peppermint", "Valintina"], ["Jinkx Monsoon", "Sasha Velour", "BenDeLaCreme", "Alyssa Edwards"], ["Sharon Needles", "Phi Phi O'Hara", "Alaska", "Monique Heart"], ["Alyssa Edwards", "Latrice Royal", "Shangela", "Aja"], ["Mayhem Miller", "Dusty Ray Bottoms", "Kalorie Karbdashian Williams", "The Vixen"], ["Thorgy Thor", "Monet Exchange", "Asia O'hara", "Bob the Drag Queen"], ["Trinity 'The Tuck' Taylor", "Valentina", "Shea Coulee", "Blair St. Clair" ]];
+var possibleAnswers = [["Trinity 'The Tuck' Taylor", "Shea Couleé", "Peppermint", "Valintina"], ["Jinkx Monsoon", "Sasha Velour", "BenDeLaCreme", "Alyssa Edwards"], ["Sharon Needles", "Phi Phi O'Hara", "Alaska", "Monique Heart"], ["Alyssa Edwards", "Latrice Royal", "Shangela", "Aja"], ["Mayhem Miller", "Dusty Ray Bottoms", "Kalorie Karbdashian Williams", "The Vixen"], ["Thorgy Thor", "Monet Exchange", "Asia O'hara", "Bob the Drag Queen"], ["Trinity 'The Tuck' Taylor", "Valentina", "Shea Coulee", "Blair St. Clair"]];
 var correctAnswers = ["c", "d", "a", "b", "c", "d", "c"];
 var correctAnswerText = ["Peppermint", "Alyssa Edwards", "Sharon Needles", "Latrice Royal", "Kalorie", "Bob", "Shea Coulee"];
 var message = "";
@@ -19,23 +19,23 @@ var timer;
 function startGame() {
 
     // set up which css to display/hide
-    $('#container').css('display','block');
-    $('#correct-incorrect').css('display','none');
-    $('#timer').css('display','none');
-    $('.question-button').css('display','none');
+    $('#container').css('display', 'block');
+    $('#correct-incorrect').css('display', 'none');
+    $('#timer').css('display', 'none');
+    $('.question-button').css('display', 'none');
     clearInterval(timer);
-    
-   
+
+
     // create start button
     var startBtn = $("<button>");
     startBtn.addClass("start-button")
     startBtn.text("Press to Start")
     $("#answers").append(startBtn);
-   
+
 
     //add start message
     $("#question").text("Press button to start your engines! And may the best woman, win!")
-    
+
     // set stats
     questionNumber = 0;
     totalRight = 0;
@@ -44,25 +44,25 @@ function startGame() {
     $(".start-button").on("click", function () {
         nextSlide();
     });
-   
+
 }
 startGame();
 
 //  make a timer countdown function that displays for a certain amount of time
- function countDownTimer() {
+function countDownTimer() {
     $("#timer").text(timerCount);
     timerCount--;
     if (timerCount === 0) {
         clearInterval(timer);
         totalWrong++;
         incorrectSlide();
-        
+
     }
 }
 
 
 function myTimer() {
-     timer = setInterval(countDownTimer, 1000);
+    timer = setInterval(countDownTimer, 1000);
 }
 
 // make an end of game funciton
@@ -75,7 +75,7 @@ function endOfGame() {
     // display stats
     $("#message").append(" \nYou got " + totalRight + " questions right!")
     $("#message").append(" \nAnd " + totalWrong + " questions wrong.")
-   
+
 
     // create do again button
     var startOverBtn = $("<button>");
@@ -103,14 +103,15 @@ function lastQuestionCheck() {
 // make function for if got answer right that shows a pic or vid and 
 function correctSlide() {
     clearInterval(timer);
-    $('#container').css('display','none');
-    $('#correct-incorrect').css('display','block');
-    
+    $('#container').css('display', 'none');
+    $('#correct-incorrect').css('display', 'block');
+
     // displays gif with DOM
     $("#image-space").html("<img src=" + images[questionNumber] + " width='400px'>")
-    
+
     // displays a congratulation message 
     $("#message").html("Congratulations! You got that one right. \nYou're a winner, Baby!").wrap('<pre />');
+
     // calls lastQuestion function if returned false
     lastQuestionCheck();
     if (lastQuestion === true) {
@@ -127,43 +128,46 @@ function correctSlide() {
 // make function for if got answer wrong that shows a pic or vid and 
 function incorrectSlide() {
     clearInterval(timer);
-    $('#container').css('display','none');
-    $('#correct-incorrect').css('display','block');
+    $('#container').css('display', 'none');
+    $('#correct-incorrect').css('display', 'block');
 
     // displays gif with DOM
     $("#image-space").html("<img src=" + images[questionNumber] + " width='400px'>")
-   // message and displays the correct answer
-   $("#message").html("Not quite. The correct answer was " + correctAnswerText[questionNumber] + ".\nNext time you better werk!").wrap('<pre />');
-   // check if last question
-   lastQuestionCheck();
-   if (lastQuestion === true) {
-     setTimeout(endOfGame, 5000);
+    
+    // message and displays the correct answer
+    $("#message").html("Not quite. The correct answer was " + correctAnswerText[questionNumber] + ".\nNext time you better werk!").wrap('<pre />');
+    
+    // check if last question
+    lastQuestionCheck();
+    if (lastQuestion === true) {
+        setTimeout(endOfGame, 5000);
 
-   } else {
-    setTimeout(questionNumber++);
-    setTimeout(nextSlide, 5000);
-   }
+    } else {
+        setTimeout(questionNumber++);
+        setTimeout(nextSlide, 5000);
+    }
 
 }
 
 
 // make a next slide function 
 function nextSlide() {
-    
-    $('#container').css('display','block');
-    $('#correct-incorrect').css('display','none');
-    $('#timer').css('display','block');
-    $('.question-button').css('display','block');
-    
-     //  set and display timer countdown
-     timerCount = 10;
-    
-     myTimer();
-   
+
+    $('#container').css('display', 'block');
+    $('#correct-incorrect').css('display', 'none');
+    $('#timer').css('display', 'block');
+    $('.question-button').css('display', 'block');
+    $('#timer').html("&nbsp");
+
+
+    //  set and display timer countdown
+    timerCount = 10;
+    myTimer();
+
 
     // display question
     $("#answers").html("");
-    
+
     $("#question").text(questions[questionNumber]);
     // display answers on buttons make 4 buttons with DOM 
     for (var i = 0; i < 4; i++) {
@@ -179,7 +183,7 @@ function nextSlide() {
         // add questions to the buttons
         questionBtn.text(possibleAnswers[questionNumber][i]);
         $("#answers").append(questionBtn);
-       
+
     }
 
 
@@ -195,7 +199,7 @@ function nextSlide() {
             // if correct go to correct function/slide and totalRight++
             correctSlide();
             totalRight++;
-        } else if  (userClicked !== correctAnswers[questionNumber]) {
+        } else if (userClicked !== correctAnswers[questionNumber]) {
             // if wrong 
             incorrectSlide();
             totalWrong++;
